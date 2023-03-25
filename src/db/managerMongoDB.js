081e21c957fa92db190b8778a3e35123497a5640
Scraylok <rodrigomonfasani@gmail.com>
2023-03-25 14:06:06 -0300

    Update managerMongoDB.js

--- src/db/managerMongoDB.js
+++ src/db/managerMongoDB.js
@@ -2,65 +2,67 @@ import mongoose from "mongoose";
 
 export class ManagerMongoDB {
 
-
-    constructor(url, collection, schema) {
+    url
+    constructor(url, collection, schema){
         this.url = url
         this.collection = collection
         this.schema = new mongoose.Schema(schema)
-        this.model = mongoose.model(this.collection, this.schema)
+        this.model = mongoose.model(this.colection, this.schema)
     }
 
     async setConnection() {
-        try {
+        try{
             await mongoose.connect(this.url)
-        } catch (error) {
+            console.log ("DB is connected")
+        }catch(error){
             return error
         }
     }
-
-    async addElements(elements) { //Agrego 1 o varios elementos
+    async addElements(elements){
         this.setConnection()
         try {
-            return await this.model.insertMany(elements)
-        } catch (error) {
+          return  await this.model.insertMany(elements)
+        }catch(error) {
             return error
+
         }
     }
 
-    async getElements() {
+    async getElements(){
         this.setConnection()
         try {
-            return await this.model.find()
-        } catch (error) {
+          return  await this.model.find()
+        }catch(error) {
             return error
+
         }
     }
-
-    async getElementById(id) { //Agrego 1 o varios elementos
+    async getElementById(id){
         this.setConnection()
         try {
-            return await this.model.findById(id)
-        } catch (error) {
+          return  await this.model.findById(id)
+        }catch(error) {
             return error
+
         }
     }
-
-    async updateElement(id, ...info) {
+    async updateElement(id,info){
         this.setConnection()
         try {
-            return await this.model.findByIdAndUpdate(id, ...info)
-        } catch (error) {
+          return  await this.model.findByIdAndUpdate(id,info)
+        }catch(error) {
             return error
+
         }
     }
-
-    async deleteElement(id) {
+    async deleteElement(id){
         this.setConnection()
         try {
-            return await this.model.findByIdAndDelete(id)
-        } catch (error) {
+          return  await this.model.findByIdAndDelete(id)
+        }catch(error) {
             return error
+
         }
     }
-
 }
+
